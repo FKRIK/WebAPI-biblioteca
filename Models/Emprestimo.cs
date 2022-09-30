ba467112a0f991db389650fa123b8755b886b491
Crissie A Giese <crissieagiese@gmail.com>
2022-09-30 20:07:01 -0300

    corrige foreignkeys em models e atualiza migration

--- Models/Emprestimo.cs
+++ Models/Emprestimo.cs
@@ -5,12 +5,21 @@ namespace WebAPI_biblioteca.Models
 {
     public class Emprestimo
     {
+
         public int Id { get; set; }
         public DateTime DataEmprestimo { get; set; }
         public DateTime DataDevolucao { get; set; }
 
-        public List<Funcionario> Funcionarios { get; set; }
+        public int FuncionarioId { get; set; }
 
-        public List<Cliente> Clientes { get; set; }
+        public int ClienteId { get; set; }
+
+        public int LivroId { get; set; }
+
+        public Boolean Finalizado { get; set; }
+
+        public Cliente Cliente;
+        public Funcionario Funcionario;
+        public Livro Livro;
     }
 }
